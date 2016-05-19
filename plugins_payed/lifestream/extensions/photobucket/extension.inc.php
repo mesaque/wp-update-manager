2a4170f3b98334444e2285990be711bf1426c599
Mesaque Silva <mesaque.silva@apiki.com>
2016-05-19 17:56:56 -0300

    removido plugins gratis

--- plugins_payed/lifestream/extensions/photobucket/extension.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Lifestream_PhotoBucketFeed extends Lifestream_PhotoFeed
-{
-	const ID	= 'photobucket';
-	const NAME	= 'Photobucket';
-	const URL	= 'http://www.photobucket.com/';
-}
-$lifestream->register_feed('Lifestream_PhotoBucketFeed');
-?>
