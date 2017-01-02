27b113b39f0a5ca3369f481b6453ea3b452d1af3
Mesaque Silva <mesaque.silva@apiki.com>
2017-01-02 16:16:17 -0200

    update gravity

--- plugins_payed/gravityforms/js/form_admin.js
+++ plugins_payed/gravityforms/js/form_admin.js
@@ -356,9 +356,10 @@ function GetRuleValues(objectType, ruleIndex, selectedFieldId, selectedValue, in
         //loading categories via AJAX
         jQuery.post( ajaxurl, {
             action:       'gf_get_address_rule_values_select',
-            address_type: field.addressType,
+            address_type: field.addressType ? field.addressType : gf_vars.defaultAddressType,
             value:        selectedValue,
-            id:           dropdownId
+            id:           dropdownId,
+            form_id:      field.formId
         }, function( selectMarkup ) {
             if( selectMarkup ) {
                 $select = jQuery( selectMarkup.trim() );
@@ -399,18 +400,17 @@ function IsAddressSelect( inputId, field ) {
         return false;
     }
 
-    switch( field.addressType ) {
-        case '':
-        case 'international':
-            selectInputId = field.id + '.6';
-            break;
-        case 'us':
-        case 'canadian':
-            selectInputId = field.id + '.4';
-            break;
+    var addressType = field.addressType ? field.addressType : gf_vars.defaultAddressType;
+
+    if( ! gf_vars.addressTypes[ addressType ] ) {
+        return false;
     }
 
-    return inputId == selectInputId;
+    var addressTypeObj = gf_vars.addressTypes[ addressType ],
+        isCountryInput = inputId == field.id + '.6',
+        isStateInput   = inputId == field.id + '.4';
+
+    return ( isCountryInput && addressType == 'international' ) || ( isStateInput && typeof addressTypeObj.states == 'object' );
 }
 
 function GetFirstRuleField(){
@@ -515,8 +515,8 @@ function TruncateRuleText(text){
 
 function gfAjaxSpinner(elem, imageSrc, inlineStyles) {
 
-    var imageSrc = typeof imageSrc == 'undefined' ? '/images/ajax-loader.gif': imageSrc;
-    var inlineStyles = typeof inlineStyles != 'undefined' ? inlineStyles : '';
+    imageSrc     = typeof imageSrc == 'undefined' || ! imageSrc ? gf_vars.baseUrl + '/images/spinner.gif': imageSrc;
+    inlineStyles = typeof inlineStyles != 'undefined' ? inlineStyles : '';
 
     this.elem = elem;
     this.image = '<img class="gfspinner" src="' + imageSrc + '" style="' + inlineStyles + '" />';
